fc765761395538b3a6ddc6a0ad056f3983599e87
yamasaki-web <146613557+yamasaki-web@users.noreply.github.com>
2025-02-26 00:08:29 +0900

    ＴＯＰ：ｍｖ自動再生追加

--- js/index.js
+++ js/index.js
@@ -7,3 +7,12 @@ $(function () {
 		pauseonhover: false
 	});
 });
+
+// mv 自動再生
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById("video");
+  if (video) {
+    video.muted = true; // 明示的にミュートを設定
+    video.play().catch(error => console.error("再生エラー:", error)); // エラーをキャッチ
+  }
+});
